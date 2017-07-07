0873cd795add3a3dc064e28fb2a65b31cf29111b
David Rolfs <Davidrolfs15@gmail.com>
2017-07-07 15:08:07 -0700

    remove edit button and add click feature to specific animal

--- app/edit-zoo.component.ts
+++ app/edit-zoo.component.ts
@@ -7,26 +7,34 @@ import { Zoo } from './zoo.model';
     <div>
         <div *ngIf="childSelectedZoo">
           <h3>{{childSelectedZoo.description}}</h3>
-          <hr>
-          <h3>Edit Zoo</h3>
+          <h3>Edit Animal</h3>
           <label>Enter Animal species:</label>
           <input [(ngModel)]="childSelectedZoo.species">
+          <br>
           <label>Enter Animal name:</label>
           <input [(ngModel)]="childSelectedZoo.name">
+          <br>
           <label>Enter Animal age:</label>
           <input [(ngModel)]="childSelectedZoo.age">
+          <br>
           <label>Enter Animal diet:</label>
           <input [(ngModel)]="childSelectedZoo.diet">
+          <br>
           <label>Enter Animal location:</label>
           <input [(ngModel)]="childSelectedZoo.location">
+          <br>
           <label>Enter Animal caretakers:</label>
           <input [(ngModel)]="childSelectedZoo.caretakers">
+          <br>
           <label>Enter Animal sex:</label>
           <input [(ngModel)]="childSelectedZoo.sex">
+          <br>
           <label>Enter Animal likes:</label>
           <input [(ngModel)]="childSelectedZoo.likes">
+          <br>
           <label>Enter Animal dislikes:</label>
           <input [(ngModel)]="childSelectedZoo.dislikes">
+          <br>
           <button (click)="doneButtonClicked()">Done</button>
         </div>
       </div>
--- app/zoo-list.component.ts
+++ app/zoo-list.component.ts
@@ -9,8 +9,27 @@ import { Zoo } from './zoo.model';
       <option value="oldAnimals">Animals over the age of 2</option>
       <option value="youngAnimals" selected="selected">Animals under the age of 2</option>
     </select>
+    <h3> Click an Animal to edit its information" </h3>
     <ul>
-      <li *ngFor="let currentZoo of childZooList | viewAge:filterByAge">{{currentZoo.species}}, {{currentZoo.name}}, {{currentZoo.age}}, {{currentZoo.diet}}, {{currentZoo.location}}, {{currentZoo.caretakers}}, {{currentZoo.sex}}, {{currentZoo.likes}}, {{currentZoo.dislikes}}<button (click)="editButtonHasBeenClicked(currentZoo)">Edit!</button>
+      <li (click)="editButtonHasBeenClicked(currentZoo)" *ngFor="let currentZoo of childZooList | viewAge:filterByAge">
+      <span class="title">Species:</span> {{currentZoo.species}}
+      <br>
+      Name: {{currentZoo.name}}
+      <br>
+      Age: {{currentZoo.age}}
+      <br>
+      Diet: {{currentZoo.diet}},
+      <br>
+      Location: {{currentZoo.location}},
+      <br>
+      Caretakers: {{currentZoo.caretakers}},
+      <br>
+      Sex: {{currentZoo.sex}},
+      <br>
+      Likes: {{currentZoo.likes}},
+      <br>
+      Dislikes: {{currentZoo.dislikes}}
+      <br>
       </li>
     </ul>
   `
