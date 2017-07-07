2db3751d37caeabeb40d237054919bed965584d4
David Rolfs <Davidrolfs15@gmail.com>
2017-07-07 11:40:54 -0700

    returns form field blank after submitted

--- app/new-zoo.component.ts
+++ app/new-zoo.component.ts
@@ -8,25 +8,35 @@ import { Zoo } from './zoo.model';
    <div>
      <label>Enter species:</label>
      <input #newZooSpecies>
+     <br>
      <label>Enter name:</label>
      <input #newZooName>
+     <br>
      <label>Enter age:</label>
      <input #newZooAge>
+     <br>
      <label>Enter diet:</label>
      <input #newZooDiet>
+     <br>
      <label>Enter location:</label>
      <input #newZooLocation>
+     <br>
      <label>Enter caretakers:</label>
      <input #newZooCaretakers>
+     <br>
      <label>Enter sex:</label>
      <input #newZooSex>
+     <br>
      <label>Enter likes:</label>
      <input #newZooLikes>
+     <br>
      <label>Enter dislikes:</label>
      <input #newZooDislikes>
+     <br>
    </div>
    <div>
-      <button (click)="submitForm(newZooSpecies.value, newZooName.value, newZooAge.value, newZooDiet.value, newZooLocation.value, newZooCaretakers.value, newZooSex.value, newZooLikes.value, newZooDislikes.value);">Add</button>
+      <button (click)="submitForm(newZooSpecies.value, newZooName.value, newZooAge.value, newZooDiet.value, newZooLocation.value, newZooCaretakers.value, newZooSex.value, newZooLikes.value, newZooDislikes.value);
+      newZooSpecies.value = '' + newZooName.value = '' + newZooAge.value = '' + newZooDiet.value = '' + newZooLocation.value = '' + newZooCaretakers.value = '' + newZooSex.value = '' + newZooLikes.value = '' + newZooDislikes.value = '';">Add</button>
     </div>
   `
 })
